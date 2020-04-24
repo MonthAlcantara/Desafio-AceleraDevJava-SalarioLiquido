e8eaada12470beb14a32becb0fc57e0b8e0f2b45
Montival Junior <montival_junior@yahoo.com.br>
2020-04-24 12:33:20 -0300

    Envio de resolução primeiro desafio aceleradev Java

---
+++ src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
@@ -0,0 +1,46 @@
+package br.com.codenation.calculadora;
+
+public class CalculadoraSalario {
+    private static final double SALARIO_MINIMO = 1039.00;
+
+    private static final double DESCONTO_INSS_FAIXA01 = 0.08;
+    private static final double DESCONTO_INSS_FAIXA02 = 0.09;
+    private static final double DESCONTO_INSS_FAIXA03 = 0.11;
+    private static final double DESCONTO_IRRF_FAIXA01 = 0.075;
+    private static final double DESCONTO_IRRF_FAIXA02 = 0.15;
+
+    public long calcularSalarioLiquido(double salarioBase) {
+        validarSalarioInformado(salarioBase);
+        double salarioDescontado = calcularDescontoInss(salarioBase);
+        return Math.round(calcularDescontoIrrf(salarioDescontado));
+    }
+
+    private double calcularDescontoInss(double salarioBase) {
+        if (salarioBase > SALARIO_MINIMO && salarioBase <= 1500.00) {
+            return salarioBase -= salarioBase * DESCONTO_INSS_FAIXA01;
+        }
+        return (salarioBase > 1500.00 && salarioBase <= 4000.00) ? (salarioBase -= salarioBase * DESCONTO_INSS_FAIXA02) : (salarioBase -= salarioBase * DESCONTO_INSS_FAIXA03);
+    }
+
+    private double calcularDescontoIrrf(double salarioBase) {
+        if (verificarIsencaoIrrf(salarioBase)) {
+            return 0.00;
+        } else {
+            if (salarioBase <= 3000.00) {
+                return salarioBase;
+            }
+            return (salarioBase > 3000.00 && salarioBase <= 6000.00) ? (salarioBase -= salarioBase * DESCONTO_IRRF_FAIXA01) : (salarioBase -= salarioBase * DESCONTO_IRRF_FAIXA02);
+        }
+    }
+
+    private void validarSalarioInformado(double salarioBase) throws NullPointerException{
+        if ((Double) salarioBase == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    private boolean verificarIsencaoIrrf(double salariobase) {
+        return (salariobase <= SALARIO_MINIMO) ? true : false;
+    }
+
+}
